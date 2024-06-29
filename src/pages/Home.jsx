0f8d5478972d3459ca3d0ddb42987b978e21e32c
AyoubNL <gmilio@live.nl>
2024-06-29 03:35:06 +0200

    NavBar Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
 
 function Home() {
+
   return (
     <>
-      <h1>Homepagina</h1>
+      <h1>Homepage</h1>
       <section>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem id libero provident! Accusantium at autem
           cumque, esse eum, fugiat ipsa ipsam quia quis ratione repudiandae sapiente sequi sit tempora voluptates.
